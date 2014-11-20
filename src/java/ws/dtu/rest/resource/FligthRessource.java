76321b5a38bbc82d05560edc2fa4ff98f89a7b68
emilhein <emilhein1@gmail.com>
2014-11-20 13:13:19 +0100

    ss

--- src/java/ws/dtu/rest/resource/FligthRessource.java
+++ src/java/ws/dtu/rest/resource/FligthRessource.java
@@ -1,6 +1,6 @@
 package ws.dtu.rest.resource;
 
-import com.sun.tools.xjc.reader.xmlschema.bindinfo.BIConversion.Static;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -8,18 +8,14 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import ws.dtu.rest.data.Flight;
 
 
-@Path("flight/{id}")
-public class FligthRessource {
-   
-    
-  static final String FLIGHT_URI = "http://localhost:8080/RestWebService/webresources/flight/";
-
+@Path("flight")
+public class FligthRessource { 
+  static final String FLIGHT_URI = "http://localhost:8080/RestWebService/webresources/flight";
   public static Flight flight = null;
 
     
@@ -31,19 +27,16 @@ public class FligthRessource {
  
    
    @GET
-   @Produces(MediaType.APPLICATION_XML)
-   public Flight getFlightXML(@PathParam("id") String id) {
-        flight.setNumber(id);
-        return flight;
+   @Produces(MediaType.TEXT_PLAIN)
+   public String getFlight(@PathParam("id") String id) {
+       
+        return "RESPONSE 2";
     }
    
-   @GET
-    public String findFlight(@QueryParam("id") int id) {
-        String url = FLIGHT_URI+"123";
-        return url;
-    }
-   
-   
+   @GET //The simplest GET request returns this 
+   public String getFlight(){
+       return "TEST RESPONSE";
+   }
    
    @PUT
     @Consumes(MediaType.APPLICATION_XML)
@@ -53,12 +46,14 @@ public class FligthRessource {
         flight.setNumber(id);
         return Response.ok().entity(flight).build();
     }
-   
- 
  
   @DELETE
     public String deleteFlight(@PathParam("id") int number) {
         return "deleted "+number;
     }
+
+  
+
+  
  
 }
